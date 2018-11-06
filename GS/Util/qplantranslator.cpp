7f317cfb6a1a1b399d6e910acfa2ff9bb9931daa
junbaih <junbaih@gmail.com>
2018-11-05 22:56:53 -0800

    added waypt items tranlation, updated GS original waypoint key value to retrieve the right json value from interop missions

--- GS/Util/qplantranslator.cpp
+++ GS/Util/qplantranslator.cpp
@@ -46,17 +46,41 @@ QString qPlanTranslator::planDirectory()
 
 QByteArray qPlanTranslator::processMission(QByteArray missionData)
 {
-    QJsonDocument jdoc = QJsonDocument::fromJson(missionData);
-    qDebug()<<jdoc.isNull()<<jdoc.isArray()<<jdoc.isEmpty();
-    QJsonObject temp = jdoc.array().at(0).toObject();
+    QJsonDocument missionDoc = QJsonDocument::fromJson(missionData);
+    //qDebug()<<missionDoc.isNull()<<missionDoc.isArray()<<missionDoc.isEmpty();
+    QJsonObject missionJObj = missionDoc.array().at(0).toObject();
     QJsonArray temp2 = QJsonDocument::fromJson(missionData).array();
     QJsonArray temp3={1,2,"plan"};
 
     qDebug()<<"jarray3"<<QJsonDocument(temp3).toJson();
     qDebug()<<"jarray3 keys"<<QJsonDocument(temp3).object().keys();
     qDebug()<<"jarray"<<temp2.isEmpty()<<temp2.last();
-    qDebug()<<"jobject"<<temp.isEmpty()<<temp.keys();
-
+   // qDebug()<<"jobject"<<temp.isEmpty()<<temp.keys();
+    Mission* pMission = new Mission(missionJObj,QJsonObject());
+    QJsonArray flyZoneWayptsJArray;
+    for(QVector2D boundary:pMission->fly_zones[0].boundary_points)
+    {
+        flyZoneWayptsJArray.append(QJsonArray{boundary[0],boundary[1]});
+    }
+    QJsonArray wayPointsJArray;
+    int i = 1;
+    for (Waypt wpt:pMission->generatedPath.waypoints)
+    {
+        wayPointsJArray.append(
+            QJsonObject{
+                        {"AMSLAltAboveTerrain",QJsonValue()},
+                        {"Altitude",wpt.coords.z()},
+                        {"AltitudeMode",0},
+                        {"type","SimpleItem"},
+                        {"autoContinue",true},
+                        {"frame",0},
+                        {"doJumpId",i},
+                        {"command",wpt.action},
+                        {"params",QJsonArray{wpt.param1,wpt.param2,wpt.param3,wpt.param4,wpt.coords.x(),wpt.coords.y(),wpt.coords.z()}}
+                    }
+                               );
+        i++;
+    }
     QJsonObject planObj
     {
         {"fileType","Plan"},
@@ -64,21 +88,31 @@ QByteArray qPlanTranslator::processMission(QByteArray missionData)
         {"version",1},
         {"rallyPoints",QJsonObject
             {
-                {"version",2},{"points",QJsonArray()}
+                {"version",2},{"points",QJsonArray{QJsonArray{38.084120,-76.254590, 20}}}
             }
         },
         {"geoFence",QJsonObject
             {
                 {"circles",QJsonArray()},
-                {"polygons",QJsonArray()},
-                {"version",1}
+                {"polygons",QJsonArray
+                    {
+                    QJsonObject
+                    {
+                        {"inclusion",true},
+                        {"polygon",flyZoneWayptsJArray},
+                        {"version",1}
+                    }
+                    }
+                },
+                {"version",2}
             }
         }
     };
+
     QJsonObject missionItem
     {
-        {"items",QJsonArray()},
-        {"plannedHomePosition",QJsonArray{38.14792,-76.427995,200}},
+        {"items",wayPointsJArray},
+        {"plannedHomePosition",QJsonArray{pMission->home_pos.x(),pMission->home_pos.y(),200}},
         {"curiseSpeed",15},
         {"firmwareType",12},  // 12 PX4, 3 Ardupilot
         {"hoverSpeed",5},
